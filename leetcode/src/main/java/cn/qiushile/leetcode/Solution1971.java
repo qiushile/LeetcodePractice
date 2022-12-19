ed1dfe1bc1515f270441aa314896cafd094949c7
qiushile <qiushile@gmail.com>
2022-12-19 21:22:15 +0800

    solve 1971

---
+++ leetcode/src/main/java/cn/qiushile/leetcode/Solution1971.java
@@ -0,0 +1,197 @@
+package cn.qiushile.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author qiushile <qiushile@sina.com>
+ * @date 2022/12/19
+ */
+public class Solution1971 {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        if (n < 0 || source < 0 || destination < 0 || source >= n || destination >= n) {
+            return false;
+        }
+
+        if (source == destination) {
+            return true;
+        }
+
+        n++;
+        source++;
+        destination++;
+
+        int links[] = new int[n];
+        int tmp;
+        for (int[] edge : edges) {
+            int left = edge[0] + 1;
+            int right = edge[1] + 1;
+            if (links[right] == left) {
+                continue;
+            }
+            if (left > right) {
+                tmp = left;
+                left = right;
+                right = tmp;
+            }
+            if (links[left] == 0 && links[right] == 0) {
+                links[right] = left;
+                continue;
+            }
+            if (links[right] == 0) {
+                links[right] = links[left];
+                continue;
+            }
+            if (links[left] == 0) {
+                int secondRight = links[right];
+                while (secondRight > left) {
+                    right = secondRight;
+                    secondRight = links[right];
+                }
+                if (secondRight == left) {
+                    continue;
+                }
+                links[left] = secondRight;
+                links[right] = left;
+                continue;
+            }
+            merge(links, left, right);
+        }
+
+        return find(links, source, destination);
+    }
+
+    private boolean find(int[] links, int left, int right) {
+        if (left == 0 || right == 0) {
+            return false;
+        }
+        if (left == right) {
+            return true;
+        }
+        if (left > right) {
+            return find(links, right, left);
+        }
+        int secondRight = links[right];
+        while (secondRight > left) {
+            right = secondRight;
+            secondRight = links[right];
+        }
+        if (left == secondRight) {
+            return true;
+        } else {
+            return find(links, secondRight, left);
+        }
+    }
+
+    private void merge(int[] links, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        if (left > right) {
+            merge(links, right, left);
+        }
+        int secondRight = links[right];
+        while (secondRight > left) {
+            right = secondRight;
+            secondRight = links[right];
+        }
+        if (secondRight == left) {
+            return;
+        }
+        if (secondRight == 0) {
+            links[right] = left;
+            return;
+        }
+        int secondLeft = links[left];
+        if (secondLeft == secondRight) {
+            links[right] = left;
+            return;
+        //} else if (secondLeft < secondRight) {
+        //    links[right] = left;
+        //    links[left] = secondRight;
+        //    merge(links, secondLeft, secondRight);
+        //    return;
+        //} else if (secondLeft > secondRight) {
+        //    links[right] = left;
+        //    merge(links, secondLeft, secondRight);
+        //    return;
+        } else {
+            links[right] = left;
+            merge(links, secondRight, left);
+            return;
+        }
+    }
+
+    // failed at 22
+    public boolean oldValidPath(int n, int[][] edges, int source, int destination) {
+
+        if (n < 0 || source < 0 || destination < 0 || source >= n || destination >= n) {
+            return false;
+        }
+
+        if (source == destination) {
+            return true;
+        }
+
+        List<Set<Integer>> setList = new ArrayList<Set<Integer>>();
+        for (int[] edge : edges) {
+            int left = edge[0];
+            int right = edge[1];
+            if (left == right) {
+                continue;
+            }
+            int leftPosition = -1;
+            int rightPosition = -1;
+            for (int i = 0; i < setList.size() && (leftPosition < 0 || rightPosition < 0); i++) {
+                Set<Integer> link = setList.get(i);
+                if (link.contains(left)) {
+                    leftPosition = i;
+                }
+                if (link.contains(right)) {
+                    rightPosition = i;
+                }
+            }
+            if (leftPosition < 0 && rightPosition < 0) {
+                Set<Integer> link = new HashSet<Integer>();
+                link.add(left);
+                link.add(right);
+                setList.add(link);
+                continue;
+            }
+            if (leftPosition == rightPosition) {
+                continue;
+            }
+            if (leftPosition >= 0 && rightPosition < 0) {
+                setList.get(leftPosition).add(right);
+                continue;
+            }
+            if (leftPosition < 0 && rightPosition >= 0) {
+                setList.get(rightPosition).add(left);
+                continue;
+            }
+            if (leftPosition > rightPosition) {
+                int temp = leftPosition;
+                leftPosition = rightPosition;
+                rightPosition = temp;
+            }
+            setList.get(leftPosition).addAll(setList.get(rightPosition));
+            setList.remove(rightPosition);
+        }
+
+        for (Set<Integer> link : setList) {
+            boolean begin = link.contains(source);
+            boolean end = link.contains(destination);
+            if (begin && end) {
+                return true;
+            }
+            if (begin || end) {
+                return false;
+            }
+        }
+
+        throw new IllegalStateException("Unable to run here!!");
+    }
+}
---
+++ leetcode/src/test/java/cn/qiushile/leetcode/Solution1971Test.java
@@ -0,0 +1,45 @@
+package cn.qiushile.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Solution1971Test {
+
+    private Solution1971 solution1971UnderTest;
+
+    @Before
+    public void setUp() {
+        solution1971UnderTest = new Solution1971();
+    }
+
+    @Test
+    public void testValidPath1() {
+        assertTrue(solution1971UnderTest.validPath(3, new int[][]{{0,1},{1,2},{2,0}}, 0, 2));
+    }
+
+    @Test
+    public void testValidPath2() {
+        assertFalse(solution1971UnderTest.validPath(6, new int[][]{{0,1},{0,2},{3,5},{5,4},{4,3}}, 0, 5));
+    }
+
+
+    @Test
+    public void testValidPath17() {
+        assertTrue(solution1971UnderTest.validPath(50,
+                new int[][]{{41,40},{9,32},{48,14},{6,44},{18,41},{41,1},{7,41},{38,41},{19,4},{16,41},{41,43},{41,22},
+                        {41,21},{9,0},{41,48},{32,36},{24,44},{39,41},{48,17},{49,15},{47,41},{19,31},{11,41},{41,23},
+                        {41,49},{45,44},{2,49},{13,41},{35,41},{30,0},{5,44},{8,0},{41,20},{41,28},{12,11},{12,41},{49,10},
+                        {19,0},{0,37},{34,41},{42,48},{27,41},{0,41},{19,44},{41,26},{41,29},{33,41},{49,46},{41,25},{3,41}}, 40, 3));
+    }
+
+    @Test
+    public void testValidPath19() {
+        assertTrue(solution1971UnderTest.validPath(10,
+                new int[][]{{2,6},{4,7},{1,2},{3,5},{7,9},{6,4},{9,8},{0,1},{3,0}}, 3, 5));
+    }
+
+
+}

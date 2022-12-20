98970eeeab421a86777f078d50e3dbd53cab27fe
qiushile <qiushile@gmail.com>
2022-12-20 14:47:25 +0800

    solve 0226: Invert Binary Tree

---
+++ leetcode/src/main/java/cn/qiushile/leetcode/Solution0226.java
@@ -0,0 +1,70 @@
+package cn.qiushile.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 226. Invert Binary Tree
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ * @author qiushile <qiushile@sina.com>
+ * @date 2022/12/20
+ */
+public class Solution0226 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return root;
+        }
+        if (root.right == null && root.left != null) {
+            root.right = invertTree(root.left);
+            root.left = null;
+        } else if (root.left == null && root.right != null) {
+            root.left = invertTree(root.right);
+            root.right = null;
+        } else {
+            TreeNode tmp = invertTree(root.left);
+            root.left = invertTree(root.right);
+            root.right = tmp;
+        }
+
+        return root;
+    }
+
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+        return root;
+    }
+
+    /**
+     * Definition for a binary tree node.
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        public TreeNode(int val) { this.val = val; }
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ leetcode/src/test/java/cn/qiushile/leetcode/Solution0226Test.java
@@ -0,0 +1,43 @@
+package cn.qiushile.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class Solution0226Test {
+
+    private Solution0226 solution0226UnderTest;
+
+    @Before
+    public void setUp() {
+        solution0226UnderTest = new Solution0226();
+    }
+
+    @Test
+    public void testInvertTree2() {
+        // Setup
+        final Solution0226.TreeNode root = new Solution0226.TreeNode(2);
+        root.left = new Solution0226.TreeNode(1);
+        root.right = new Solution0226.TreeNode(3);
+        // Run the test
+        final Solution0226.TreeNode result = solution0226UnderTest.invertTree(root);
+
+        // Verify the results
+        assertEquals(3, result.left.val);
+        assertEquals(1, result.right.val);
+    }
+
+    @Test
+    public void testInvertTree3() {
+        // Setup
+        final Solution0226.TreeNode root = null;
+
+        // Run the test
+        final Solution0226.TreeNode result = solution0226UnderTest.invertTree(root);
+
+        // Verify the results
+        assertNull(result);
+    }
+}

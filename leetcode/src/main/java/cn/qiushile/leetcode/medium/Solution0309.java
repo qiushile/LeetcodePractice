c01c2353e4604943511ab42e5d694e190a29c721
qiushile <qiushile@gmail.com>
2022-12-23 10:02:00 +0800

    solve 0309: Best Time to Buy and Sell Stock with Cooldown - fail at test 202

---
+++ leetcode/src/main/java/cn/qiushile/leetcode/medium/Solution0309.java
@@ -0,0 +1,62 @@
+package cn.qiushile.leetcode.medium;
+
+/**
+ * 309. Best Time to Buy and Sell Stock with Cooldown
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times) with the following restrictions:
+ * After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
+ * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+ * Example 1:
+ * Input: prices = [1,2,3,0,2]
+ * Output: 3
+ * Explanation: transactions = [buy, sell, cooldown, buy, sell]
+ * Example 2:
+ * Input: prices = [1]
+ * Output: 0
+ * Constraints:
+ * 1 <= prices.length <= 5000
+ * 0 <= prices[i] <= 1000
+ *
+ * @author qiushile <qiushile@sina.com>
+ * @date 2022/12/23
+ */
+public class Solution0309 {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        if (len <= 1) {
+            return 0;
+        }
+        int index = 0;
+        int startPrice;
+        int earn = 0;
+        while (index <= len - 1) {
+            while (index < len - 1 && prices[index] >= prices[index + 1]) {
+                index++;
+            }
+            startPrice = prices[index];
+            index++;
+            while (index < len - 1 && prices[index] <= prices[index + 1]) {
+                index++;
+            }
+            if (index < len - 2 && prices[index + 2] > prices[index + 1]) {
+                // earn more in next grow, only process one fluctuation
+                if (prices[index + 2] - startPrice > prices[index + 2] - prices[index + 1] + prices[index - 1] - startPrice
+                        && prices[index + 2] - startPrice > prices[index] - startPrice) {
+                    earn += prices[index + 2] - startPrice;
+                    index = index + 2;
+                } else if (prices[index + 2] - prices[index + 1] > prices[index] - prices[index - 1]) {
+                    earn += prices[index - 1] - startPrice;
+                } else {
+                    earn += prices[index] - startPrice;
+                    index = index + 2;
+                }
+                continue;
+            }
+            if (index <= len - 1 && prices[index] > startPrice) {
+                earn += prices[index] - startPrice;
+                continue;
+            }
+        }
+        return earn;
+    }
+}
---
+++ leetcode/src/test/java/cn/qiushile/leetcode/medium/Solution0309Test.java
@@ -0,0 +1,51 @@
+package cn.qiushile.leetcode.medium;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution0309Test {
+
+    private Solution0309 solution0309UnderTest;
+
+    @Before
+    public void setUp() {
+        solution0309UnderTest = new Solution0309();
+    }
+
+    @Test
+    public void testMaxProfit001() {
+        assertEquals(3, solution0309UnderTest.maxProfit(new int[]{1,2,3,0,2}));
+    }
+
+    @Test
+    public void testMaxProfit002() {
+        assertEquals(0, solution0309UnderTest.maxProfit(new int[]{1}));
+    }
+
+    @Test
+    public void testMaxProfit003() {
+        assertEquals(0, solution0309UnderTest.maxProfit(new int[]{2,1}));
+    }
+
+    @Test
+    public void testMaxProfit131() {
+        assertEquals(1, solution0309UnderTest.maxProfit(new int[]{2,1,2,0,1}));
+    }
+
+    @Test
+    public void testMaxProfit192() {
+        assertEquals(6, solution0309UnderTest.maxProfit(new int[]{6,1,3,2,4,7}));
+    }
+
+    @Test
+    public void testMaxProfit193() {
+        assertEquals(10, solution0309UnderTest.maxProfit(new int[]{2,1,4,5,2,9,7}));
+    }
+
+    @Test
+    public void testMaxProfit202() {
+        assertEquals(10, solution0309UnderTest.maxProfit(new int[]{1,3,5,4,3,7,6,9,2,4}));
+    }
+}

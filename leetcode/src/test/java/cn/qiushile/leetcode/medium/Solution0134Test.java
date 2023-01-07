b1d06fee0ea43e9186394d301c71151e19f42aa6
qiushile <qiushile@gmail.com>
2023-01-07 09:04:23 +0800

    solve 0134. Gas Station - failed: Memory Limit Exceeded

---
+++ leetcode/src/test/java/cn/qiushile/leetcode/medium/Solution0134Test.java
@@ -0,0 +1,26 @@
+package cn.qiushile.leetcode.medium;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution0134Test {
+
+    private Solution0134 solution0134UnderTest;
+
+    @Before
+    public void setUp() throws Exception {
+        solution0134UnderTest = new Solution0134();
+    }
+
+    @Test
+    public void testCanCompleteCircuit01() {
+        assertEquals(3, solution0134UnderTest.canCompleteCircuit(new int[]{1,2,3,4,5}, new int[]{3,4,5,1,2}));
+    }
+
+    @Test
+    public void testCanCompleteCircuit02() {
+        assertEquals(-1, solution0134UnderTest.canCompleteCircuit(new int[]{2,3,4}, new int[]{3,4,3}));
+    }
+}

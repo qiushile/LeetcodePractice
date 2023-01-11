250798f485881238a47078ebf3c9432a0ff62f0b
qiushile <qiushile@gmail.com>
2023-01-11 10:08:50 +0800

    solve 1443. Minimum Time to Collect All Apples in a Tree - accepted：add annotation

--- leetcode/src/main/java/cn/qiushile/leetcode/medium/Solution1443.java
+++ leetcode/src/main/java/cn/qiushile/leetcode/medium/Solution1443.java
@@ -41,23 +41,32 @@ import java.util.Set;
  */
 public class Solution1443 {
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        // all the nodes from the apple to root
         Set<Integer> ans = new HashSet<>();
+        // the nodes whose parents are not processed
         Queue<Integer> queue = new LinkedList<>();
+        // processed nodes
         Set<Integer> exists = new HashSet<>();
+        // add the root node
         exists.add(0);
+        // mark all apples
         for (int i = 0; i < hasApple.size(); i++) {
             if (hasApple.get(i)) {
                 ans.add(i);
                 queue.add(i);
             }
         }
+        // no apple
         if (ans.isEmpty()) {
             return 0;
         }
+        // (k, v) -> (child, parent)
         Map<Integer, Integer> father = new HashMap<>();
         for (int[] edge : edges) {
+            // up: parent, down: child
             int up = edge[0];
             int down = edge[1];
+            // if the down already exists, mark it up
             if (exists.contains(edge[1]) && !exists.contains(edge[0])) {
                 up = edge[1];
                 down = edge[0];
@@ -66,11 +75,13 @@ public class Solution1443 {
                 ans.add(up);
                 queue.add(up);
             } else {
+                // not yet connected to apple route
                 father.put(down, up);
             }
             exists.add(down);
             exists.add(up);
         }
+        // process the father nodes, util meet a processed father
         while (!queue.isEmpty()) {
             Integer next = queue.poll();
             while (father.containsKey(next)) {
@@ -81,6 +92,7 @@ public class Solution1443 {
                 }
             }
         }
+        // remove the count of root node
         return (ans.size() - 1) * 2;
     }
 }

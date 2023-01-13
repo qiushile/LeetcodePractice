024adf5f84a4a2d66ea360ed7e635d646913f678
qiushile <qiushile@gmail.com>
2023-01-14 01:06:57 +0800

    solve 1819. Number of Different Subsequences GCDs - failed：Time Limit Exceeded at 30/39

---
+++ leetcode/src/main/java/cn/qiushile/leetcode/hard/Solution1819.java
@@ -0,0 +1,79 @@
+package cn.qiushile.leetcode.hard;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+/**
+ * 1819. Number of Different Subsequences GCDs
+ * Hard
+ * 330
+ * 35
+ * Companies
+ * You are given an array nums that consists of positive integers.
+ * The GCD of a sequence of numbers is defined as the greatest integer that divides all the numbers in the sequence evenly.
+ * For example, the GCD of the sequence [4,6,16] is 2.
+ * A subsequence of an array is a sequence that can be formed by removing some elements (possibly none) of the array.
+ * For example, [2,5,10] is a subsequence of [1,2,1,2,4,1,5,10].
+ * Return the number of different GCDs among all non-empty subsequences of nums.
+ * Example 1:
+ * Input: nums = [6,10,3]
+ * Output: 5
+ * Explanation: The figure shows all the non-empty subsequences and their GCDs.
+ * The different GCDs are 6, 10, 3, 2, and 1.
+ * Example 2:
+ * Input: nums = [5,15,40,5,6]
+ * Output: 7
+ * Constraints:
+ * 1 <= nums.length <= 105
+ * 1 <= nums[i] <= 2 * 105
+ * @author qiushile <qiushile@sina.com>
+ * @date 2023/1/14
+ */
+public class Solution1819 {
+    public int countDifferentSubsequenceGCDs(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return 1;
+        }
+        Set<Integer> ans = new HashSet<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> addList = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = n - 1; i >= 0; i--) {
+            list.add(nums[i]);
+            ans.add(nums[i]);
+        }
+        while (!list.isEmpty()) {
+            Integer curr = list.poll();
+            for (Integer num : list) {
+                Integer g = gcd(curr, num);
+                if (!ans.contains(g)) {
+                    ans.add(g);
+                    addList.add(g);
+                }
+            }
+            list.addAll(addList);
+            addList.clear();
+        }
+        return ans.size();
+    }
+
+    private Integer gcd(int a, int b) {
+        if (a == b) {
+            return a;
+        }
+        while (b != 0) {
+            a %= b;
+            if (a == 0) {
+                return b;
+            }
+            b %= a;
+            if (b == 0) {
+                return a;
+            }
+        }
+        return a;
+    }
+}
---
+++ leetcode/src/test/java/cn/qiushile/leetcode/hard/Solution1819Test.java
@@ -0,0 +1,26 @@
+package cn.qiushile.leetcode.hard;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution1819Test {
+
+    private Solution1819 solution1819UnderTest;
+
+    @Before
+    public void setUp() throws Exception {
+        solution1819UnderTest = new Solution1819();
+    }
+
+    @Test
+    public void testCountDifferentSubsequenceGCDs01() {
+        assertEquals(5, solution1819UnderTest.countDifferentSubsequenceGCDs(new int[]{6,10,3}));
+    }
+
+    @Test
+    public void testCountDifferentSubsequenceGCDs02() {
+        assertEquals(7, solution1819UnderTest.countDifferentSubsequenceGCDs(new int[]{5,15,40,5,6}));
+    }
+}
